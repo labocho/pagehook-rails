ed90e49797803a32a1e525a8c1feb692f86f92d5
labocho <labocho@penguinlab.jp>
2014-12-24 18:43:33 +0900

    update pagehook to 0.1.2

--- app/assets/javascripts/pagehook_rails/pagehook.js
+++ app/assets/javascripts/pagehook_rails/pagehook.js
@@ -1,84 +1,82 @@
-(function() {
-  var Pagehook,
-    __slice = [].slice;
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self),o.Pagehook=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
 
-  this.Pagehook = Pagehook = (function() {
-    Pagehook.GLOBAL_HOOK_NAME = "@global";
+// constructor
+var Pagehook = function() {
+  this.definitions = {};
+  this.handler = this.handlerUnbound.bind(this);
+};
 
-    Pagehook.ATTRIBUTE_NAME = "data-pagehook";
+// constant
+Pagehook.GLOBAL_HOOK_NAME = "@global";
+Pagehook.ATTRIBUTE_NAME = "data-pagehook";
 
-    Pagehook.register = function() {
-      var args, _ref;
-      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      return (_ref = this.instance).register.apply(_ref, args);
-    };
+// static methods
+Pagehook.register = function(name_or_map, func) {
+  this.instance.register(name_or_map, func);
+};
 
-    Pagehook.dispatch = function() {
-      var args, _ref;
-      args = 1 <= arguments.length ? __slice.call(arguments, 0) : [];
-      return (_ref = this.instance).dispatch.apply(_ref, args);
-    };
+Pagehook.dispatch = function(name, arg) {
+  this.instance.dispatch(name, arg);
+};
 
-    function Pagehook() {
-      this.definitions = {};
-      this.handler = (function(_this) {
-        return function() {
-          return _this.handlerUnbound();
-        };
-      })(this);
+// Pagehook.register "name", (arg)-> ...
+// // or
+// Pagehoook.register
+//   name: (arg)->
+Pagehook.prototype.register = function(name_or_map, func) {
+  var name;
+
+  if (typeof(name_or_map) === "string") {
+    if (!this.definitions[name_or_map]) {
+      this.definitions[name_or_map] = [];
     }
+    this.definitions[name_or_map].push(func);
+  } else {
+    for (name in name_or_map) {
+      this.register(name, name_or_map[name]);
+    }
+  }
+};
 
-    Pagehook.prototype.register = function(name_or_map, func) {
-      var name, _base, _results;
-      if (typeof name_or_map === "string") {
-        return ((_base = this.definitions)[name_or_map] || (_base[name_or_map] = [])).push(func);
-      } else {
-        _results = [];
-        for (name in name_or_map) {
-          func = name_or_map[name];
-          _results.push(this.register(name, func));
-        }
-        return _results;
-      }
-    };
+// Pagehook.dispatch("name", {foo: 1, bar: 2})
+Pagehook.prototype.dispatch = function(name, arg) {
+  if (this.definitions[name]) {
+    this.definitions[name].forEach(function(func) {
+      func(arg);
+    });
+  } else {
+    if (name !== Pagehook.GLOBAL_HOOK_NAME) {
+      console.log("Pagehook for " + name + " is undefined");
+    }
+  }
+};
 
-    Pagehook.prototype.dispatch = function(name, arg) {
-      var func, _i, _len, _ref, _results;
-      if (this.definitions[name]) {
-        _ref = this.definitions[name];
-        _results = [];
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          func = _ref[_i];
-          _results.push(func(arg));
-        }
-        return _results;
-      }
-    };
+// Event handler for DOMContentLoaded or page:change (turbolinks)
+// Use `handler` property instead of this
+Pagehook.prototype.handlerUnbound = function() {
+  var elements, i, e, name, arg;
 
-    Pagehook.prototype.handlerUnbound = function() {
-      var arg, e, name, _i, _len, _ref, _results;
-      this.dispatch(Pagehook.GLOBAL_HOOK_NAME);
-      _ref = document.querySelectorAll("[" + Pagehook.ATTRIBUTE_NAME + "]");
-      _results = [];
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        e = _ref[_i];
-        name = e.getAttribute(Pagehook.ATTRIBUTE_NAME);
-        arg = this.isBlank(e.textContent) ? void 0 : JSON.parse(e.textContent);
-        _results.push(this.dispatch(name, arg));
-      }
-      return _results;
-    };
+  this.dispatch(Pagehook.GLOBAL_HOOK_NAME);
 
-    Pagehook.prototype.isBlank = function(string) {
-      return !!(string.match(/^\s*$/));
-    };
+  elements = document.querySelectorAll("[" + Pagehook.ATTRIBUTE_NAME + "]");
+  for (i = 0; i < elements.length; i++) {
+    e = elements[i];
+    name = e.getAttribute(Pagehook.ATTRIBUTE_NAME);
+    arg = this.isBlank(e.textContent) ? undefined : JSON.parse(e.textContent);
+    this.dispatch(name, arg);
+  }
+};
 
-    return Pagehook;
+Pagehook.prototype.isBlank = function(string) {
+  return !!(string.match(/^\s*$/));
+};
 
-  })();
+// instanciate singleton object
+Pagehook.instance = new Pagehook();
+Pagehook.handler = Pagehook.instance.handler;
 
-  this.Pagehook.instance = new Pagehook();
+module.exports = Pagehook;
 
-  this.Pagehook.handler = this.Pagehook.instance.handler;
-
-}).call(this);
+},{}]},{},[1])(1)
+});
